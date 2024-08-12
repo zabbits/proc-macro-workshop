0c858c76f94ab545b4ef404aad13b35bd4eb87f6
zabbits <zabbits@outlook.com>
2024-08-12 15:06:02 +0800

    builder 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _ = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    TokenStream::new().into()
 }
