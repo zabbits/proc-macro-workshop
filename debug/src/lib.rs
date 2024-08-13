9f15f07fbd8851e365490eb8f03e2c3d3030e15b
zabbits <zabbits@outlook.com>
2024-08-13 16:41:30 +0800

    debug 01

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,8 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _ = parse_macro_input!(input as DeriveInput);
+    TokenStream::new().into()
 }
