7944f856ae470b059a6965c529fb57cbe8e14dfc
zabbits <zabbits@outlook.com>
2024-08-13 17:01:25 +0800

    debug 02

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,27 @@
-use proc_macro2::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
-    TokenStream::new().into()
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    match input.data {
+        syn::Data::Struct(syn::DataStruct { fields, .. }) => {
+            let dbg_fields = fields.iter().map(|f| {
+                let name = format!("{}", f.ident.as_ref().unwrap());
+                let ident = &f.ident;
+                quote! { field(#name, &self.#ident) }
+            });
+            quote! {
+                impl std::fmt::Debug for #ident {
+                    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+                        f.debug_struct(stringify!(#ident))
+                            #(.#dbg_fields)*
+                            .finish()
+                    }
+                }
+            }.into()
+        }
+        _ => unimplemented!(),
+    }
 }
