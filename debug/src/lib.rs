b477b3903e1d469cd5a70073250890b7c1baa008
zabbits <zabbits@outlook.com>
2024-08-14 09:58:36 +0800

    debug 04

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,13 +1,15 @@
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{Data, DataStruct, DeriveInput, Expr, ExprLit, Field, GenericParam, Generics, Lit, Meta};
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+    let mut input = syn::parse_macro_input!(input as DeriveInput);
+    add_traits_bound(&mut input.generics);
     // println!("{:#?}", input);
     let ident = input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
     match input.data {
-        syn::Data::Struct(syn::DataStruct { fields, .. }) => {
+        Data::Struct(DataStruct { fields, .. }) => {
             let dbg_fields = fields.iter().map(|f| {
                 let name = format!("{}", f.ident.as_ref().unwrap());
                 let ident = &f.ident;
@@ -19,7 +21,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 }
             });
             quote! {
-                impl std::fmt::Debug for #ident {
+                impl #impl_generics std::fmt::Debug for #ident #ty_generics #where_clause {
                     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
                         f.debug_struct(stringify!(#ident))
                             #(.#dbg_fields)*
@@ -32,16 +34,16 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     }
 }
 
-fn get_debug_attr(f: &syn::Field) -> Option<String> {
+fn get_debug_attr(f: &Field) -> Option<String> {
     f.attrs.iter().find_map(|attr| {
         // meta must be a NamedValue
-        if let syn::Meta::NameValue(nv) = &attr.meta {
+        if let Meta::NameValue(nv) = &attr.meta {
             if !nv.path.is_ident("debug") {
                 return None;
             }
             match &nv.value {
-                syn::Expr::Lit(syn::ExprLit {
-                    lit: syn::Lit::Str(litstr),
+                Expr::Lit(ExprLit {
+                    lit: Lit::Str(litstr),
                     ..
                 }) => Some(litstr.value()),
                 _ => None,
@@ -50,4 +52,12 @@ fn get_debug_attr(f: &syn::Field) -> Option<String> {
             None
         }
     })
+}
+
+fn add_traits_bound(generics: &mut Generics) {
+    for param in &mut generics.params {
+        if let GenericParam::Type(type_param) = param {
+            type_param.bounds.push(syn::parse_quote!(std::fmt::Debug));
+        }
+    }
 }
